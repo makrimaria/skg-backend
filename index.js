9e81778ce1ac1267c30a4d9245989a02574da536
maria makri <maria.m@compliancesolutions.com>
2024-07-09 20:12:24 +0300

    added crud

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+//imports
 import express from "express"
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 
-const PORT = 3000; 
+//configuration
+const PORT = 5000;
 const MONGO_URI = "mongodb+srv://mariaskgcode:naikalispera@backend.l0hxls2.mongodb.net/?retryWrites=true&w=majority&appName=backend"
 
+//initialize express app
 const app = express()
+app.use(express.json()); //middleware to parse JSON bodies of incoming requests
 
+//connection to mongodb
 mongoose.connect(MONGO_URI).then(() => {
     console.log("Database is connected successfully!");
-    app.listen(PORT, () => {
+    app.listen(PORT, () => { //if the connection is successful it starts the server and logs a success message
         console.log("Server is running on port: " + PORT);
     });
-}).catch((error) => {
+}).catch((error) => { //logs any connection errors
     console.log(error);
 });
 
-
-mongoose.connection.on("connected", async () => {
-    //TODO: edw tha elegksw an to collection einai adeio kai tha kanw insert ta data pou thelw
-
+//check connection and initialize data
+mongoose.connection.on("connected", async () => { // <- event listener for successful mongodb connection
     const moviesCount = await MoviesModel.countDocuments();
-    if(moviesCount == 0) {
+    if (moviesCount == 0) { //checks if there are any movies in the collection, and if not, it calls the inserInitialMovies() function to insert the initial movie data
         insertInitialMovies();
     }
 })
 
+//define movie schema
 const moviesSchema = new mongoose.Schema({
     id: Number,
     title: String,
@@ -35,8 +39,10 @@ const moviesSchema = new mongoose.Schema({
     image: String
 });
 
+//define movies model
 const MoviesModel = mongoose.model("movies", moviesSchema);
 
+//the insertInitialMovies() function
 const insertInitialMovies = async () => {
     try {
 
@@ -101,7 +107,69 @@ const insertInitialMovies = async () => {
     }
 }
 
+//Get all movies from db
+app.get('/movies', async (req, res) => {
+
+    try {
+        const movies = await MoviesModel.find();
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//Get a movie by the object id 
+app.get('/movies/:id', async (req, res) => {
+    try {
+        const movie = await MoviesModel.findById(req.params.id);
+        if(!movie) return res.status(404).json({ message: "Movie not found" });
+        res.status(200).json(movie)
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 
+//Get a movie by our custom id field
+app.get('/movies/custom/:id', async (req, res) => {
+    try {
+        const movie = await MoviesModel.findOne({ id: req.params.id });
+        if(!movie) return res.status(404).json({ message: "Movie not found" });
+        res.status(200).json(movie)
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
+//Delete a movie by id
+app.delete('/movies/:id', async (req, res) => {
+    try {
+        const deletedMovie = await MoviesModel.findByIdAndDelete(req.params.id);
+        if(!deletedMovie) return res.status(404).json({ message: "Movie not found" });
+        res.status(200).json({ message: "Movie deleted successfully"})
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
+//Post a new movie
+app.post('/movies', async (req, res) => {
+    try {
+        const movie = new MoviesModel({
+            _id: new mongoose.Types.ObjectId(),
+            id: req.body.id,
+            title: req.body.title,
+            director: req.body.director,
+            release_year: req.body.release_year,
+            genre: req.body.genre,
+            rating: req.body.rating,
+            image: req.body.image
+        });
+
+        movie.save();
+        res.status(201).json(movie);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
